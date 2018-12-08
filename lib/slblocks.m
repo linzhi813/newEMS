e5b9c4216d112bdce6b835aac6aa8031f31b110c
linzhi813 <linzhi_chan@163.com>
2018-12-08 16:58:48 +0800

    commit lib files from simulink project

---
+++ lib/slblocks.m
@@ -0,0 +1,6 @@
+function blkStruct=slblocks
+%Information for Simulink Library Browser
+Browser(1).Library='mylibrary';
+Browser(1).Name='mylibrary';
+Browser(1).IsFlat=1; %no subsystem
+blkStruct.Browser=Browser;
